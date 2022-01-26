643487b7681fd809d06d102b810d31f5d7a3e72a
zhangr75 <zhangr75@mcmaster.ca>
2022-01-26 17:27:09 -0500

    test new frontend and database connection

---
+++ client_test/databaseconnection.php
@@ -0,0 +1,27 @@
+<?php
+class Database
+{
+    private $servername = "localhost";
+    private $dbName = "capstone";
+    private $username = "root";
+    private $password = "password";
+    public $conn;
+
+    public function getConnection(){
+        try{
+            $this->conn = new PDO("mysql:host=$this->servername;dbname=$this->dbName", $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            
+            //status = '1' for connect to the database properly status = '0' for connect to the databse failed
+            $response['status'] = '1';
+            $response['connection'] = $this->conn;
+            return $response;
+        }catch(PDOException $e){
+            $response['status'] = '0';
+            $response['message'] = $e->getMessage();
+            return $response;
+        }
+        
+    }
+}   
+?>
