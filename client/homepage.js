e71886b9d7d511ef4162942f82c4dbfcdd5920ba
zhangr75 <zhangr75@mcmaster.ca>
2022-02-09 21:43:42 -0500

    add homepage as index and rename exist index to index1.html and php js css for homepage

---
+++ client/homepage.js
@@ -0,0 +1,57 @@
+var markers = [];
+function initMap() {
+    //set up map object by initializing the zoom index and the position: lat, lag
+        map = new google.maps.Map(document.getElementById("map-canvas"),{
+        zoom: 17, 
+        mapTypeControl: true,
+        mapTypeControlOptions: {
+         style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+         position: google.maps.ControlPosition.LEFT_BOTTOM,
+         },
+        fullscreenControl: false, 
+        center: {lat:43.26264877904412,  lng: -79.91941510031896}});
+}
+
+function showMarker(){
+    clearmarker();
+    let building = $('#selectbuilding').val();
+    var la, lo;
+    var data ='';
+    $.ajax({
+         type:"GET",
+         url:"php/addmarker.php",
+         data: {"building":building},
+         dataType: "html",
+         async : false,
+         success:function(result){
+         data = eval("("+result+")");
+         $.each(data, function (i,item) {
+            la = item.latitude;
+            lo = item.longitude;
+         })
+         }
+     })
+    for (var i = 0; i < data.length; i++){
+        var buildingname = data[i].name;
+        markers [i] = new google.maps.Marker({
+            map: map,
+            url: "http://3.21.69.46/Capstone/client/bookings/get-booking-info-page.html"
+        });
+        var la = data[i].latitude;
+        var lo = data[i].longitude;
+        position = new google.maps.LatLng(la,lo);
+        markers[i].setPosition(position);
+        map.setCenter(position);
+        map.setZoom(17);
+        google.maps.event.addListener(markers[i], 'click', function() {
+            window.location.href = this.url;
+        });
+    }
+}
+
+function clearmarker() {
+    for (var i = 0; i < markers.length; i++ ) {
+      markers[i].setMap(null);
+    }
+    markers.length = 0;
+  }
---
+++ client/php/addmarker.php
@@ -0,0 +1,28 @@
+<?php 
+    //Database class for connect to the database
+    include_once 'databaseconnection.php';
+    
+    $building = $_GET["building"];
+    $i = 0;
+    //Connect to database by using PDO
+    $database = new Database();
+    $db = $database->getConnection();
+    if($db['status'] == '0'){
+        echo "Connection to database failed: " . $db['message'];
+    }else{
+        try {
+            $conn = $db['connection'];
+            $query = "select * from buildings where name ='$building' ";
+            $request = $conn->prepare($query);
+            $request->execute();
+            while($row =$request->fetch(PDO::FETCH_ASSOC)){
+                $arrays[$i]=$row;
+                $i++;
+            }        
+            echo json_encode($arrays);    
+        }
+        catch (Exception $e) {
+            die("something went wrong".$e->getMessage());
+        }
+    }
+?>
