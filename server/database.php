7f237d1f75fd66a382460609d29a0afc37341af3
Jessica de Leeuw <jessica.deleeuw2@gmail.com>
2022-02-10 22:40:14 -0500

    Fixed db setup for server

--- server/database.php
+++ server/database.php
@@ -4,7 +4,7 @@ class Database {
     private $host = "localhost";
     private $db_name = "capstone";
     private $username = "root";
-    private $password = "";
+    private $password = "password";
     public $conn;
 
 
